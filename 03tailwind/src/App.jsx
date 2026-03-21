638e9401d969720ef092e6c19ed749917fafc41f
sweety62 <myselfsweety1@gmail.com>
2026-03-21 22:45:33 +0530

    creating button using tailwindcss

---
+++ 03tailwind/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+    <button className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">
+      click me
+    </button>
+      </>
+  )
+}
+export default App;
+                 
